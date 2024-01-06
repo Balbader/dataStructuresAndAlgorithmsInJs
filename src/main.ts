01c7677a2f4cc319f80b2e64a39583e95d70683e
balbader <basilalbapro@gmail.com>
2024-01-06 03:12:24 +0100

    file deletion

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { CUser } from "./CUser";
-import { CWebDev } from "./CWebDev";
-
-const Basil = new CUser("Basil", "Alba", 40);
-const Mimi = new CWebDev("Mimi", "Anderson", 36, "System76");
-
-Basil.printUserInfo();
-Mimi.printWebDevInfo()
