def4e3c2b16ba04f256cd549bfc471718d78ae16
balbader <basilalbapro@gmail.com>
2024-07-13 12:56:10 +0200

    file deletio9n

--- baseball-game.js
+++
@@ -1,4 +0,0 @@
-var addScoreToArr = function (score, arr) { return arr.push(score); };
-var removePreviousScoreFromArr = function (arr) { return arr.pop(); };
-var finalArray = new Array();
-var array = ['5', '2', 'C', 'D', '+'];
--- baseball-game.ts
+++
@@ -1,5 +0,0 @@
-const addScoreToArr = (score: string, arr: string[]) => arr.push(score);
-const removePreviousScoreFromArr = (arr: string[]) => arr.pop();
-
-let finalArray = new Array();
-let array = ['5', '2', 'C', 'D', '+'];
