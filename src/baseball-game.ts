540addc9885f1a7593f86b6167718026a4f79ae7
balbader <basilalbapro@gmail.com>
2024-07-13 12:56:05 +0200

    add src folder

---
+++ src/baseball-game.ts
@@ -0,0 +1,5 @@
+const addScoreToArr = (score: string, arr: string[]) => arr.push(score);
+const removePreviousScoreFromArr = (arr: string[]) => arr.pop();
+
+let finalArray = new Array();
+let array = ['5', '2', 'C', 'D', '+'];
