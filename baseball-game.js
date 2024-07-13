e026ffddfdf24995fc655fbe8bc0f676785c71fe
balbader <basilalbapro@gmail.com>
2024-07-13 12:54:49 +0200

    WIP

---
+++ baseball-game.js
@@ -0,0 +1,4 @@
+var addScoreToArr = function (score, arr) { return arr.push(score); };
+var removePreviousScoreFromArr = function (arr) { return arr.pop(); };
+var finalArray = new Array();
+var array = ['5', '2', 'C', 'D', '+'];
